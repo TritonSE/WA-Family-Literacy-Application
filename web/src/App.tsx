9c4f930dd504d4614b7ca98b61f6054730c22fb5
David Cruz <david@typokign.com>
2020-12-31 16:26:11 -0800

    Add web starter code

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <p>TODO</p>
+    </div>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default App;
