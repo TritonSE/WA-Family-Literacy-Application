15aa68d4bf5eb817b46d45da8302079bde093afc
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2021-01-13 09:00:30 +0530

    react navigation

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,44 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function HomeScreen() {
+  return(
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+}
+
+function ChatScreen() {
+  return(
+    <View>
+      <Text>Chat</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return(
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
 
 const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>TODO</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Chat" component={ChatScreen}/>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Settings" component={Settings}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
