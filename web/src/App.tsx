84d0310e4d7d3d65d8ff095171739c9cb8013570
Anshul <anbirla@ucsd.edu>
2021-03-24 16:51:40 -0700

    applied davids changes

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import { Navbar } from './components/Navbar';
 import './App.css';
+import { Communication } from './pages/Communication';
+import { Analytics } from './pages/Analytics';
+import { UploadBooks } from './pages/UploadBooks';
+import { Manage } from './pages/Manage';
 
 const App: React.FC = () => {
   return (
-    <Navbar/>
+    <Router>
+      <Navbar/>
+
+      <Switch>
+        <Route path="/communication">
+          <Communication/>
+        </Route>
+        <Route path="/analytics">
+          <Analytics/>
+        </Route>
+        <Route path="/upload">
+          <UploadBooks/>
+        </Route>
+        <Route path="/manage">
+          <Manage/>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -1,50 +1,24 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  NavLink,
-} from 'react-router-dom';
-import { Communication } from '../pages/Communication';
-import { Analytics } from '../pages/Analytics';
-import { UploadBooks } from '../pages/UploadBooks';
-import { Manage } from '../pages/Manage';
+import { NavLink } from 'react-router-dom';
 import '../css/Navbar.css';
 
 export const Navbar: React.FC = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <NavLink activeClassName="active" to="/communication">Communication</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" to="/analytics">Analytics</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" to="/upload">Upload Books</NavLink>
-          </li>
-          <li>
-            <NavLink activeClassName="active" to="/manage">Manage</NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      <Switch>
-        <Route path="/communication">
-          <Communication />
-        </Route>
-        <Route path="/analytics">
-          <Analytics />
-        </Route>
-        <Route path="/upload">
-          <UploadBooks />
-        </Route>
-        <Route path="/manage">
-          <Manage />
-        </Route>
-      </Switch>
-    </Router>
+    <nav>
+      <ul>
+        <li>
+          <NavLink activeClassName="active" to="/communication">Communication</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/analytics">Analytics</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/upload">Upload Books</NavLink>
+        </li>
+        <li>
+          <NavLink activeClassName="active" to="/manage">Manage</NavLink>
+        </li>
+      </ul>
+    </nav>
   );
 };
