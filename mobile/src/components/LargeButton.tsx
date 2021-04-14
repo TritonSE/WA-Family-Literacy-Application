b7538b35c5fb2daea07312ff8a489090439085bc
Rohan Puthukudy <rohan@gleebee.com>
2021-04-14 01:10:26 -0700

    fixed lint errors

--- mobile/src/components/LargeButton.tsx
+++ mobile/src/components/LargeButton.tsx
@@ -6,54 +6,54 @@ import { TextStyles } from '../styles/TextStyles';
 type LargeButtonProps = { text: string, onPress: () => void };
 
 export const LargeButton: React.FC<LargeButtonProps> = ({ text, onPress }) => {
-	const [active, setActive] = React.useState(false);
+  const [active, setActive] = React.useState(false);
 
-	return (
-		<Pressable 
-			onPressIn={() => setActive(true)} 
-			onPressOut={
-				() => {
-				onPress();
-				setActive(false);
-				}
-			} 
-			style={[styles.button, active ? styles.activeButton : styles.inactiveButton]} >
-			<Text style={[TextStyles.h3, active ? styles.activeText : styles.inactiveText]}>
-				{text}
-			</Text>
-		</Pressable>
-	);
-}
+  return (
+    <Pressable 
+      onPressIn={() => setActive(true)} 
+      onPressOut={
+        () => {
+          onPress();
+          setActive(false);
+        }
+      } 
+      style={[styles.button, active ? styles.activeButton : styles.inactiveButton]} >
+      <Text style={[TextStyles.h3, active ? styles.activeText : styles.inactiveText]}>
+        {text}
+      </Text>
+    </Pressable>
+  );
+};
 
 const styles = StyleSheet.create({
-	button: {
-		height: 43,
-		width: 298,
-		borderWidth: 2,
-		borderRadius: 5,
-		shadowColor: 'black',
-    	shadowOpacity: 0.16,
-    	shadowRadius: 6,
-    	shadowOffset: { width: 0, height: 3 },
-		marginLeft: 40,
-    	marginRight: 40,
-    	marginTop: 15,
-		paddingVertical: 10,
-	},
-	activeButton: {
-		borderColor: 'white',
-		backgroundColor: 'white',
-	},
-	inactiveButton: {
-		borderColor: Colors.orange,
-		backgroundColor: Colors.orange,
-	},
-	activeText: {
-		textAlign: 'center',
-		color: Colors.orange,
-	},
-	inactiveText: {
-		textAlign: 'center',
-		color: 'white',
-	},
+  button: {
+    height: 43,
+    width: 298,
+    borderWidth: 2,
+    borderRadius: 5,
+    shadowColor: 'black',
+    shadowOpacity: 0.16,
+    shadowRadius: 6,
+    shadowOffset: { width: 0, height: 3 },
+    marginLeft: 40,
+    marginRight: 40,
+    marginTop: 15,
+    paddingVertical: 10,
+  },
+  activeButton: {
+    borderColor: 'white',
+    backgroundColor: 'white',
+  },
+  inactiveButton: {
+    borderColor: Colors.orange,
+    backgroundColor: Colors.orange,
+  },
+  activeText: {
+    textAlign: 'center',
+    color: Colors.orange,
+  },
+  inactiveText: {
+    textAlign: 'center',
+    color: 'white',
+  },
 });
--- mobile/src/screens/ProfileScreen.tsx
+++ mobile/src/screens/ProfileScreen.tsx
@@ -7,105 +7,105 @@ import { ButtonGroup } from '../components/ButtonGroup';
 import { LargeButton } from '../components/LargeButton';
 
 const SavedTab: React.FC = () => {
-	return <Text> Saved </Text>;
+  return <Text> Saved </Text>;
 };
 
 const SettingsTab: React.FC = () => {
-	return <Text> Settings </Text>;
+  return <Text> Settings </Text>;
 };
 
 const MoreInfoTab: React.FC = () => {
-	const [value, onChangeText] = React.useState('');
+  const [value, onChangeText] = React.useState('');
 
-	return (
-		<View style={{ alignSelf: 'center', width: 298, alignItems: 'center'}}>
-			<Text style={[TextStyles.h3, {marginBottom: 20}]} >Social Media</Text>
-			<View style={styles.socialRow}>
-				<TouchableOpacity onPress={async () => await Linking.openURL("https://twitter.com/WordsAliveSD")}>
-					<Image style={styles.socialPic} source={require('../../assets/images/twitter.png')}/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={async () => await Linking.openURL("https://instagram.com/wordsalivesd")}>
-				<Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/instagram.png')}/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={async () => await Linking.openURL("https://www.facebook.com/WordsAliveSD")}>
-				<Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/facebook.png')}/>
-				</TouchableOpacity>
-				<TouchableOpacity onPress={async () => await Linking.openURL("https://www.tiktok.com/@wordsalivesd")}>
-				<Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/tiktok.png')}/>
-				</TouchableOpacity>
-			</View>
-			<LargeButton text="Donate" onPress={async () => await Linking.openURL("https://www.wordsalive.org/donate")}/>
-			<LargeButton text="Become a Volunteer" onPress={async () => await Linking.openURL("https://www.wordsalive.org/becomeavolunteer")}/>
-			<Text style={[TextStyles.h1, {textAlign: 'center', marginTop: 20}]}>
-				Interested in our Program?
-			</Text>
-			<Text style={[TextStyles.caption3, {textAlign: 'center', marginTop: 10}]}>
-				Please send us a message!
-			</Text>
-			<TextInput
-				style={styles.textBox}
-				multiline
-				onChangeText={text => onChangeText(text)}
-				value={value}
-			/>
-			<LargeButton text="Send" onPress={async () => await Linking.openURL(`mailto:amanda@wordsalive.org?subject=Family Literacy App Contact Form&body=${value}`)} />
-		</View>
-	);
+  return (
+    <View style={{ alignSelf: 'center', width: 298, alignItems: 'center'}}>
+      <Text style={[TextStyles.h3, {marginBottom: 20}]} >Social Media</Text>
+      <View style={styles.socialRow}>
+        <TouchableOpacity onPress={async () => await Linking.openURL("https://twitter.com/WordsAliveSD")}>
+          <Image style={styles.socialPic} source={require('../../assets/images/twitter.png')}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={async () => await Linking.openURL("https://instagram.com/wordsalivesd")}>
+          <Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/instagram.png')}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={async () => await Linking.openURL("https://www.facebook.com/WordsAliveSD")}>
+          <Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/facebook.png')}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={async () => await Linking.openURL("https://www.tiktok.com/@wordsalivesd")}>
+          <Image style={[styles.socialPic, styles.notLeftPic]} source={require('../../assets/images/tiktok.png')}/>
+        </TouchableOpacity>
+      </View>
+      <LargeButton text="Donate" onPress={async () => await Linking.openURL("https://www.wordsalive.org/donate")}/>
+      <LargeButton text="Become a Volunteer" onPress={async () => await Linking.openURL("https://www.wordsalive.org/becomeavolunteer")}/>
+      <Text style={[TextStyles.h1, {textAlign: 'center', marginTop: 20}]}>
+        Interested in our Program?
+      </Text>
+      <Text style={[TextStyles.caption3, {textAlign: 'center', marginTop: 10}]}>
+        Please send us a message!
+      </Text>
+      <TextInput
+        style={styles.textBox}
+        multiline
+        onChangeText={text => onChangeText(text)}
+        value={value}
+      />
+      <LargeButton text="Send" onPress={async () => await Linking.openURL(`mailto:amanda@wordsalive.org?subject=Family Literacy App Contact Form&body=${value}`)} />
+    </View>
+  );
 };
 
-const TabScreens: { [index:string] : any } = {
-	'saved': <SavedTab/>,
-	'settings': <SettingsTab/>,
-	'moreInfo': <MoreInfoTab/>,
+const TabScreens: { [index: string]: React.FC } = {
+  'saved': <SavedTab/>,
+  'settings': <SettingsTab/>,
+  'moreInfo': <MoreInfoTab/>,
 };
 
 /**
  * Right tab on navbar for profile menu
  */
 export const ProfileScreen: React.FC = () => {
-	const [selectedTab, selectTab] = React.useState('saved');
-	
- 	return (
-		<ScrollView>
-     	<View style={styles.heading}>
+  const [selectedTab, selectTab] = React.useState('saved');
+  
+  return (
+    <ScrollView>
+      <View style={styles.heading}>
         <Svg height="100%" width="100%" viewBox="0 0 1 1">
-        	<Circle cx="0.5" cy="-0.5" r="0.8" stroke={Colors.orange} fill={Colors.orange} />
+          <Circle cx="0.5" cy="-0.5" r="0.8" stroke={Colors.orange} fill={Colors.orange} />
         </Svg>
-    	</View>
-			<View style={styles.buttonGroup}>
-				<ButtonGroup btn1="saved" btn2="settings" btn3="moreInfo" onBtnChange={(btn) => {selectTab(btn)}} />
-			</View>
-			{ TabScreens[selectedTab] }
-		</ScrollView>
- 	);
+      </View>
+      <View style={styles.buttonGroup}>
+        <ButtonGroup btn1="saved" btn2="settings" btn3="moreInfo" onBtnChange={(btn) => {selectTab(btn);}} />
+      </View>
+      { TabScreens[selectedTab] }
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-	heading: {
-    	color: Colors.orange,
-    	height: 350,
-	},
-	socialRow: {
-		flexDirection: 'row',
-		justifyContent: 'space-between',
-		marginBottom: 20,
-	},
-	socialPic: {
-		height: 32,
-		width: 32,
-	},
-	notLeftPic: {
-		marginLeft: 20,
-	},
-	textBox: {
-		height: 300,
-		width: 280,
-		borderWidth: 1,
-		borderColor: Colors.orange,
-		padding: 10,
-		marginTop: 10,
-	},
-	buttonGroup: {
-		alignItems: 'center',
-	},
+  heading: {
+    color: Colors.orange,
+    height: 350,
+  },
+  socialRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  socialPic: {
+    height: 32,
+    width: 32,
+  },
+  notLeftPic: {
+    marginLeft: 20,
+  },
+  textBox: {
+    height: 300,
+    width: 280,
+    borderWidth: 1,
+    borderColor: Colors.orange,
+    padding: 10,
+    marginTop: 10,
+  },
+  buttonGroup: {
+    alignItems: 'center',
+  },
 });
