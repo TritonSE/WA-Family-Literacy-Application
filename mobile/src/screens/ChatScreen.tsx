5783d297a7df39b23ed503b61d642b0e7591a5d9
RohithKasar <rkasar@ucsd.edu>
2021-04-22 16:04:59 -0700

    Hyperlink and spacing added

--- mobile/src/screens/ChatScreen.tsx
+++ mobile/src/screens/ChatScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Linking, Text, View, StyleSheet } from 'react-native';
 import { I18nContext } from '../context/I18nContext';
 import { TextStyles } from '../styles/TextStyles';
 import { Colors } from '../styles/Colors';
@@ -10,43 +10,53 @@ import { Colors } from '../styles/Colors';
 export const ChatScreen: React.FC = () => {
   const i18nCtx = useContext(I18nContext);
   return (
-    <>
-      <View style={styles.title}>
-        <Text style={TextStyles.h2}>Chat is coming soon, until then:</Text>
-      </View>
-      <View style={styles.container}>
-        <Text style={TextStyles.h1}> {i18nCtx.t('letsTalk')}</Text> 
-    
-        <Text></Text>
-        <Text style={TextStyles.h2}> 
-          {i18nCtx.t('tel')}: <Text style={TextStyles.body1}>858.274.9673</Text>
-        </Text>
-        <Text style={TextStyles.h2}> 
-          {i18nCtx.t('fax')}: <Text style={TextStyles.body1}>858.274.9673</Text>
-        </Text>  
-        <Text style={TextStyles.h2}> 
-          {i18nCtx.t('days')}: <Text style={TextStyles.body1}>{i18nCtx.t('hours')}</Text>
-        </Text>
-        <Text></Text>
-        <Text style={TextStyles.h2}> 
-          {i18nCtx.t('emailUs')}: <Text style={TextStyles.body1}>info@wordsalive.org</Text>
-        </Text>
-        <Text></Text>
-        <Text style={TextStyles.h2}> 
-          {i18nCtx.t('address')}: <Text style={TextStyles.body1}>5111 Santa Fe Street Suite 219 San Diego, California, 92081, United States</Text>
-        </Text>
-      </View>
-    </>
+        <View style={styles.container}>
+      <Text style={TextStyles.h1}>Live chat is coming soon</Text>
+      <Text style={TextStyles.h2}>Until then...</Text>
+
+      {/*Use empty <Text/>s to add double vertical space where necessary*/}
+      <Text/>
+
+      <Text style={TextStyles.h1}>{i18nCtx.t('letsTalk')}</Text>
+
+      <Text/>
+      <Text/>
+      <Text/>
+      <Text/>
+
+
+      <Text style={TextStyles.h2}>
+        {i18nCtx.t('tel')}: <Text style={[TextStyles.body1, styles.link]} onPress={() => Linking.openURL('tel:+18582749673')}>+1 858.274.9673</Text>
+      </Text>
+      <Text style={TextStyles.h2}>
+          {i18nCtx.t('fax')}: <Text style={TextStyles.body1}>+1 858.274.9673</Text>
+      </Text>
+      <Text style={TextStyles.h2}>
+        {i18nCtx.t('days')}: <Text style={TextStyles.body1}>{i18nCtx.t('hours')}</Text>
+      </Text>
+
+      <Text/>
+
+      <Text style={TextStyles.h2}>
+        {i18nCtx.t('emailUs')}: <Text style={[TextStyles.body1, styles.link]} onPress={() => Linking.openURL('mailto:info@wordsalive.org')}>info@wordsalive.org</Text>
+      </Text>
+
+      <Text/>
+
+      <Text style={TextStyles.h2}>
+        {i18nCtx.t('address')}:
+      </Text>
+
+      {/*We would use separate <Text> tags instead of \n's here, but we want the entire address to be selectable as one element so it can be copied or opened in Maps*/}
+      <Text style={TextStyles.body1} selectable={true}>5111 Santa Fe Street Suite 219{'\n'}San Diego, California, 92081{'\n'}United States</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  title: {
-    flex: 0,
-    justifyContent: 'center',
-    backgroundColor: Colors.orange,
-    paddingLeft: 24,
-    paddingTop: '30%',
+  link: {
+    color: Colors.link,
+    textDecorationLine: 'underline',
   },
   container: {
     flex: 1,
