663aa65b11fc13927d3c78c63de4dc4d295b93a4
Anshul-Birla <anshulbirla@gmail.com>
2021-04-27 15:18:41 -0700

    boilerplate code

---
+++ web/src/pages/BookWizard/TabContentPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+export const TabContentPage: React.FC = () => {
+
+  const [video, setVideo] = useState< string | null>(null);
+  const [body, setBody] = useState< string >("");
+  return (
+
+
+
+
+
+  )
+
+
+}
+
