79f73137ca83f222ca0dc142745c448e616637d2
Anshul-Birla <anshulbirla@gmail.com>
2021-04-27 15:29:13 -0700

    more boiled plate code for contant change

--- web/src/pages/BookWizard/TabContentPage.tsx
+++ web/src/pages/BookWizard/TabContentPage.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+type TabConentPageProps = {
+  onContentChange: (TabContentModel)=> {},
+}
 
-
-export const TabContentPage: React.FC = () => {
+export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange}) => {
 
   const [video, setVideo] = useState< string | null>(null);
   const [body, setBody] = useState< string >("");
+  useEffect( () => {
+    onContentChange({
+      video: video, 
+      body: body,
+    })
+  }, [body, video])
   return (
 
 
