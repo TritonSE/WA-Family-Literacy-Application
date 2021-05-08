fc50792f1912a9bf924d6843e363768ff20d1ffb
David Cruz <david@typokign.com>
2021-05-07 17:15:51 -0700

    icons

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,6 +1,14 @@
 export default {
-  name: 'Words-Alive',
+  name: "Let's Read with Words Alive!",
+  slug: 'words-alive',
+  icon: './assets/images/logo.png',
   version: '1.0.0',
+  android: {
+    package: 'org.wordsalive.letsread',
+    adaptiveIcon: {
+      foregroundImage: './assets/images/logo-small.png'
+    }
+  },
   extra: {
     BASE_URL: process.env.BASE_URL,
   },
