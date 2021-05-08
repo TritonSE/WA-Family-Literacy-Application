5cccb3f917f54860f7addc4af05b1569a6f4a804
David Cruz <david@typokign.com>
2021-05-08 10:14:52 -0700

    Add app and site branding

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,13 +1,17 @@
 export default {
-  name: "Let's Read with Words Alive!",
+  name: "Let's Read",
   slug: 'words-alive',
-  icon: './assets/images/logo.png',
+  icon: './assets/images/icons/ios.png',
   version: '1.0.0',
   android: {
     package: 'org.wordsalive.letsread',
     adaptiveIcon: {
-      foregroundImage: './assets/images/logo-small.png'
-    }
+      foregroundImage: './assets/images/icons/android.png',
+    },
+  },
+  ios: {
+    bundleIdentifier: 'org.wordsalive.letsread',
+    icon: './assets/images/icons/ios.png',
   },
   extra: {
     BASE_URL: process.env.BASE_URL,
