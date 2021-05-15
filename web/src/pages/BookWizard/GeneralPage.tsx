8e372d3e8258c9c891704e6dead8d0ea5a772939
Anshul <anbirla@ucsd.edu>
2021-05-15 15:51:44 -0700

    fixed spacing and styling

--- web/src/pages/BookWizard/GeneralPage.tsx
+++ web/src/pages/BookWizard/GeneralPage.tsx
@@ -62,42 +62,48 @@ export const GeneralPage: React.FC<GeneralPageProps> = ({onTitleChange, onAuthor
   return(
     <div>
       <div className = {wizardStyles.mainDivElement}>
-        <div className={styles.imageTitle}>
+        <div className={styles.pageContainer}>
+          <div>
+            <div className={styles.imageTitle}>
           Book Cover (IMG)
-        </div>
-        <div 
-          className = {styles.dropZone}
-          onDragOver = {handleOnDragOver}
-          onDrop = {handleOnDrop}
-          onClick = { () => {
-            if (fileInput.current != null) {
-              fileInput.current.click();
-            }}}>
+            </div>
+            <div 
+              className = {styles.dropZone}
+              onDragOver = {handleOnDragOver}
+              onDrop = {handleOnDrop}
+              onClick = { () => {
+                if (fileInput.current != null) {
+                  fileInput.current.click();
+                }}}>
 
-          <div className = {styles.dropZoneContent}>
-            {image != null ? 
-              <img src={previewUrl} alt='image' className={styles.dropZoneImage}/> : <p className={styles.dropZoneText}>Upload Image Here</p>}
-            <input type="file"
-              ref = {fileInput} hidden
-              onChange = { (e) => {
-                if(e.target != null && e.target.files != null) {
-                  handleFile(e.target.files[0]);}}}/>
+              <div className = {styles.dropZoneContent}>
+                {image != null ? 
+                  <img src={previewUrl} alt='image' className={styles.dropZoneImage}/> : <p className={styles.dropZoneText}>Upload Image Here</p>}
+                <input type="file"
+                  ref = {fileInput} hidden
+                  onChange = { (e) => {
+                    if(e.target != null && e.target.files != null) {
+                      handleFile(e.target.files[0]);}}}/>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className = {styles.inputElement}>
-          <div className = {wizardStyles.inputBoxTitle}>
-            Title
-          </div>
-          <div>
-            <input className = {wizardStyles.inputBox} value={title} type="text" onChange={ (e) => setTitle(e.target.value)}></input>
-          </div>
-        </div>
-        <div className = {styles.inputElement}>
-          <div className = {wizardStyles.inputBoxTitle}>
-            Author
-          </div>
-          <div>
-            <input className = {wizardStyles.inputBox} value={author} type="text" onChange={ (e) => setAuthor(e.target.value)}></input>
+          <div className={styles.inputElementContainer}>
+            <div className = {styles.inputElement}>
+              <div className = {wizardStyles.inputBoxTitle}>
+              Title
+              </div>
+              <div>
+                <input className = {styles.inputBox} value={title} type="text" onChange={ (e) => setTitle(e.target.value)}></input>
+              </div>
+            </div>
+            <div className = {styles.inputElement}>
+              <div className = {wizardStyles.inputBoxTitle}>
+              Author
+              </div>
+              <div>
+                <input className = {styles.inputBox} value={author} type="text" onChange={ (e) => setAuthor(e.target.value)}></input>
+              </div>
+            </div>
           </div>
         </div>
       </div>
