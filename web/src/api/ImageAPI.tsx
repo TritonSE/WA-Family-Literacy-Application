47791d35300e2c13ef93297a6648f2fc7b86c290
Anshul <anbirla@ucsd.edu>
2021-05-15 17:37:39 -0700

    lint

--- web/src/api/ImageAPI.tsx
+++ web/src/api/ImageAPI.tsx
@@ -1,29 +1,29 @@
 import axios, { AxiosInstance, AxiosRequestConfig, CancelTokenSource } from 'axios';
  
 export class ImageAPI {
-    client: AxiosInstance;
-    cancelToken: CancelTokenSource;
-    constructor(baseURL:string) {
-        this.client = axios.create({baseURL:baseURL});
-        this.cancelToken = axios.CancelToken.source();
-    }
+  client: AxiosInstance;
+  cancelToken: CancelTokenSource;
+  constructor(baseURL: string) {
+    this.client = axios.create({baseURL:baseURL});
+    this.cancelToken = axios.CancelToken.source();
+  }
 
-    async uploadImage(body: Uint8Array, contentType:string): Promise<string> {
-        const headers = {
-            'Content-Type': contentType
-        }
+  async uploadImage(body: Uint8Array, contentType: string): Promise<string> {
+    const headers = {
+      'Content-Type': contentType
+    };
         
 
-        const requestConfig:AxiosRequestConfig  = {
-            headers: headers,
-            cancelToken: this.cancelToken.token
-        }
+    const requestConfig: AxiosRequestConfig  = {
+      headers: headers,
+      cancelToken: this.cancelToken.token
+    };
         
-        const res = await this.client.post('/images', body, requestConfig);
-        return res.data
-    } 
+    const res = await this.client.post('/images', body, requestConfig);
+    return res.data;
+  } 
 
-    cancel():void {
-        this.cancelToken.cancel();
-    }
+  cancel(): void {
+    this.cancelToken.cancel();
+  }
 }
