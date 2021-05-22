5688ea9966940ac868209c403d84939d851c03fd
David Cruz <david@typokign.com>
2021-05-22 14:26:03 -0700

    Update iOS build process to build unsigned IPA

---
+++ mobile/ios/build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+pod install
+
+xcodebuild \
+  -workspace LetsRead.xcworkspace \
+  -scheme LetsRead \
+  -sdk iphoneos \
+  -configuration Release \
+  archive \
+  -archivePath $PWD/build/lets-read.xcarchive
+
+xcodebuild \
+  -exportArchive \
+  -archivePath $PWD/build/lets-read.xcarchive \
+  -exportOptionsPlist ExportOptions.plist \
+  -exportPath $PWD/build
