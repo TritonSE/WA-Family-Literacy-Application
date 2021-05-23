a1cac23ab3912621fc9500a9dfd89308e9275d55
David Cruz <david@typokign.com>
2021-05-22 20:51:46 -0700

    Wrap auth navigator in orange background to prevent screen flashing

--- mobile/src/navigation/AuthNavigator.tsx
+++ mobile/src/navigation/AuthNavigator.tsx
@@ -3,20 +3,27 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { WelcomeScreen } from '../screens/login/WelcomeScreen';
 import { LoginScreen } from '../screens/login/LoginScreen';
 import { SignUpScreen } from '../screens/login/SignUpScreen';
+import { View } from 'react-native';
+import { Colors } from '../styles/Colors';
 
 const Stack = createStackNavigator();
 
 export const AuthNavigator: React.FC = () => {
   return (
-    <Stack.Navigator
-      initialRouteName="Welcome"
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="Welcome" component={WelcomeScreen}/>
-      <Stack.Screen name="Login" component={LoginScreen}/>
-      <Stack.Screen name="SignUp" component={SignUpScreen}/>
-    </Stack.Navigator>
+    <View style={{
+      flex: 1,
+      backgroundColor: Colors.orange,
+    }}>
+      <Stack.Navigator
+        initialRouteName="Welcome"
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="SignUp" component={SignUpScreen}/>
+      </Stack.Navigator>
+    </View>
   );
 };
