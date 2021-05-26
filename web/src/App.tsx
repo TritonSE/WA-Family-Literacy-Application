7e4b4ee139f01efed98e4c59991ec519c139fa74
David Cruz <david@typokign.com>
2021-05-26 12:11:47 -0700

    Add exact path to /books/new

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
             <PrivateRoute path="/analytics">
               <AnalyticsPage />
             </PrivateRoute>
-            <PrivateRoute path="/books">
+            <PrivateRoute exact path="/books">
               <UploadBooksPage />
             </PrivateRoute>
             <PrivateRoute exact path="/books/new">
