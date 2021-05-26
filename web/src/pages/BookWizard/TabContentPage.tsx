fe1493407a6717306ccce941983ce366939a739b
David Cruz <david@typokign.com>
2021-05-25 22:27:22 -0700

    Fix ESLint warnings

--- web/src/pages/BookWizard/TabContentPage.tsx
+++ web/src/pages/BookWizard/TabContentPage.tsx
@@ -23,7 +23,7 @@ export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange, 
   const [body, setBody] = useState< string >("");
   useEffect( () => {
     onContentChange({
-      video: video, 
+      video: video,
       body: body,
     });
   }, [body, video]);
@@ -36,11 +36,13 @@ export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange, 
 
 
   // function to generate a new custom upload adapter for ckeditor
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   function CustomUploadAdapter( editor: any ): any {
     editor.plugins.get( 'FileRepository' ).createUploadAdapter = ( loader: any ) => {
       return new ImageUploadAdapter( loader );
     };
   }
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 
   const editorConfiguration = {
     toolbar: [ 'heading','|','bold','italic','strikethrough','bulletedList','numberedList', '|', 'blockQuote','imageUpload','insertTable','undo','redo'],
@@ -48,14 +50,15 @@ export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange, 
   };
 
 
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   return (
-    <div>      
+    <div>
       <div className={wizardStyles.mainDivElement}>
         <div className = {styles.videoText}>
         Video
         </div>
-      
-        <input 
+
+        <input
           type="text"
           className={styles.inputField}
           value={video}
@@ -68,7 +71,7 @@ export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange, 
         <CKEditor
           editor= { Editor }
           config = { editorConfiguration }
-          data = {body} 
+          data = {body}
           onChange= { (event: any, editor: any) => {
             const data = editor.getData();
             setBody(data);
@@ -76,5 +79,6 @@ export const TabContentPage: React.FC<TabConentPageProps> = ( {onContentChange, 
       </div>
     </div>
   );
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 };
 
