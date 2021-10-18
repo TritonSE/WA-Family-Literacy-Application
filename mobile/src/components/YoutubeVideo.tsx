d92dfdd0dc4d0e90a23b37d84a28a1873051ef9a
Philip Zhang <code@philipzha.ng>
2021-10-18 16:49:53 -0700

    working offline indicator

--- mobile/src/components/YoutubeVideo.tsx
+++ mobile/src/components/YoutubeVideo.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Platform } from 'react-native';
-import YoutubePlayer from 'react-native-youtube-iframe';
+import React from "react";
+import { Platform, Text } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+import NetInfo from "@react-native-community/netinfo";
 
 type YoutubeVideoProps = {
   url: string;
@@ -8,29 +9,53 @@ type YoutubeVideoProps = {
   height: number;
 };
 
-const VIDEO_ID_REGEX = new RegExp('^(?:https?:)?//[^/]*(?:youtube(?:-nocookie)?.com|youtu.be).*[=/]([-\\w]{11})(?:\\?|=|&|$)');
+const VIDEO_ID_REGEX = new RegExp(
+  "^(?:https?:)?//[^/]*(?:youtube(?:-nocookie)?.com|youtu.be).*[=/]([-\\w]{11})(?:\\?|=|&|$)"
+);
 
 /**
  * Wrapper for react-native-youtube-iframe/YoutubePlayer to:
  *  - automatically parse the ID from a youtube.com or youtu.be URL
  *  - fix a bug on Android when navigating away from a screen with a YoutubePlayer
  */
-export const YoutubeVideo: React.FC<YoutubeVideoProps> = ({ url, width, height }) => {
+export const YoutubeVideo: React.FC<YoutubeVideoProps> = ({
+  url,
+  width,
+  height,
+}) => {
   const urlMatch = url.match(VIDEO_ID_REGEX);
   if (!urlMatch) {
     return <></>;
   }
   const id = urlMatch[1];
+  const [isConnected, setIsConnected] = React.useState(false);
 
-  return <YoutubePlayer
-    height={height}
-    width={width}
-    videoId={id}
-    webViewStyle={{
-      opacity: 0.99, // fix for crash on Android when navigating to a new screen, see https://github.com/LonelyCpp/react-native-youtube-iframe/issues/110
-    }}
-    webViewProps={{
-      androidLayerType: Platform.OS === 'android' && Platform.Version <= 22 ? 'hardware' : 'none', // same as above
-    }}
-  />;
+  React.useEffect(() => {
+    // Subscribe
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+    });
+
+    // Unsubscribe on unmount
+    return unsubscribe;
+  });
+
+  if (isConnected)
+    return (
+      <YoutubePlayer
+        height={height}
+        width={width}
+        videoId={id}
+        webViewStyle={{
+          opacity: 0.99, // fix for crash on Android when navigating to a new screen, see https://github.com/LonelyCpp/react-native-youtube-iframe/issues/110
+        }}
+        webViewProps={{
+          androidLayerType:
+            Platform.OS === "android" && Platform.Version <= 22
+              ? "hardware"
+              : "none", // same as above
+        }}
+      />
+    );
+  return <Text>Not connected to internet</Text>;
 };
