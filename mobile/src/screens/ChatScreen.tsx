a770dc51b21c0021edd945b0381a3a31a792ceba
Philip Zhang <code@philipzha.ng>
2021-11-20 09:29:48 -0800

    style more help screen + new message input

--- mobile/src/screens/ChatScreen.tsx
+++ mobile/src/screens/ChatScreen.tsx
@@ -7,7 +7,8 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
-} from 'react-native';
+  ScrollView,
+} from "react-native";
 import { I18nContext } from '../context/I18nContext';
 import { TextStyles } from '../styles/TextStyles';
 import { Colors } from '../styles/Colors';
@@ -15,6 +16,7 @@ import { ChatAPI } from '../api/ChatAPI';
 import { ChatRoom, Message } from '../models/Chat';
 import { AuthContext } from '../context/AuthContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const chatAPI = new ChatAPI();
 
@@ -27,6 +29,7 @@ export const ChatScreen: React.FC = () => {
   const [messages, setMessages] = useState<Message[]>([]);
   const [messageText, setMessageText] = useState<string>('');
   const [chatRoomData, setChatRoomData] = useState<ChatRoom>();
+  const [showMoreHelp, setShowMoreHelp] = useState(false);
   const i18nCtx = useContext(I18nContext);
 
   const onMessagesChange = (changedMessages: Message[]): void => {
@@ -71,11 +74,11 @@ export const ChatScreen: React.FC = () => {
       // Create a new room if no previous roomId
       newRoomId = await chatAPI.createRoom(auth.user);
       setRoomId(newRoomId);
-      AsyncStorage.setItem("chatRoomId", newRoomId);
+      AsyncStorage.setItem('chatRoomId', newRoomId);
     }
     if (newRoomId && auth.user) {
       chatAPI.sendMessage(newRoomId, messageText, auth.user?.name);
-      setMessageText("");
+      setMessageText('');
     }
   };
 
@@ -84,15 +87,25 @@ export const ChatScreen: React.FC = () => {
   };
 
   return (
-    <>
-      <View style={styles.container}>
-        {messages.map(({ id, text, from }) => {
-          return (
-            <Text key={id}>
-              {text}, from: {from}
-            </Text>
-          );
-        })}
+    <SafeAreaView style={styles.contentContainer}>
+      <View style={styles.headerContainer}>
+        <TouchableOpacity onPress={() => setShowMoreHelp(true)}>
+          <Text style={TextStyles.heading2}>{i18nCtx.t("needMoreHelp")}</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.chatContainer}>
+        <ScrollView>
+          {messages.map(({ id, text, from }) => {
+            
+            return (
+              <Text key={id}>
+                {text}, from: {from}
+              </Text>
+            );
+          })}
+        </ScrollView>
+
         {!roomId && (
           <Text>Have any questions or concerns? Send us a message.</Text>
         )}
@@ -101,75 +114,152 @@ export const ChatScreen: React.FC = () => {
             <Text>Leave a rating.</Text>
           </TouchableOpacity>
         )}
-        <TextInput value={messageText} onChangeText={setMessageText} />
-        <TouchableOpacity onPress={sendMessage}>
-          <Text>send</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={[styles.container, { display: 'none' }]}>
-        <View style={styles.logoContainer}>
-          <Image
-            source={require('../../assets/images/logo-white.png')}
-            style={styles.logo}
+        <View style={styles.newMessageContainer}>
+          <TextInput
+            value={messageText}
+            onChangeText={setMessageText}
+            style={[styles.messageInput, TextStyles.caption3]}
+            placeholder="Enter a message"
           />
+          <TouchableOpacity
+            onPress={sendMessage}
+            style={styles.sendButtonContainer}
+          >
+            <Image
+              style={styles.sendIcon}
+              source={require("../../assets/images/paper-plane-solid.png")}
+            />
+          </TouchableOpacity>
         </View>
-
-        <Text style={TextStyles.heading1}>{i18nCtx.t('liveChat')}</Text>
-        <Text style={TextStyles.heading2}>{i18nCtx.t('untilThen')}</Text>
-
-        {/*Use empty <Text/>s to add double vertical space where necessary*/}
-        <Text />
-
-        <Text style={TextStyles.heading1}>{i18nCtx.t('letsTalk')}</Text>
-
-        <Text />
-
-        <Text style={TextStyles.heading2}>
-          {i18nCtx.t('tel')}:{' '}
-          <Text
-            style={[styles.body, styles.link]}
-            onPress={() => Linking.openURL('tel:+18582749673')}
-          >
-            +1 858.274.9673
-          </Text>
-        </Text>
-        <Text style={TextStyles.heading2}>
-          {i18nCtx.t('days')}:{' '}
-          <Text style={styles.body}>{i18nCtx.t('hours')}</Text>
-        </Text>
-
-        <Text />
-
-        <Text style={TextStyles.heading2}>
-          {i18nCtx.t('emailUs')}:{' '}
-          <Text
-            style={[styles.body, styles.link]}
-            onPress={() => Linking.openURL('mailto:info@wordsalive.org')}
-          >
-            info@wordsalive.org
-          </Text>
-        </Text>
-
-        <Text />
-
-        <Text style={TextStyles.heading2}>{i18nCtx.t('address')}:</Text>
-
-        {/*We would use separate <Text> tags instead of \n's here, but we want the entire address to be selectable as one element so it can be copied or opened in Maps*/}
-        <Text style={styles.body} selectable={true}>
-          5111 Santa Fe Street Suite 219{'\n'}San Diego, California, 92109{'\n'}
-          United States
-        </Text>
       </View>
-    </>
+
+      {showMoreHelp ? (
+        <View style={styles.moreHelpContainer}>
+          <Text />
+
+          <View style={styles.logoContainer}>
+            <Image
+              source={require("../../assets/images/logo-white.png")}
+              style={styles.logo}
+            />
+          </View>
+          {/*Use empty <Text/>s to add double vertical space where necessary*/}
+          <View>
+            <Text style={TextStyles.heading1}>{i18nCtx.t("letsTalk")}</Text>
+
+            <Text />
+
+            <Text style={TextStyles.heading2}>
+              {i18nCtx.t("tel")}:{" "}
+              <Text
+                style={[styles.body, styles.link]}
+                onPress={() => Linking.openURL("tel:+18582749673")}
+              >
+                +1 858.274.9673
+              </Text>
+            </Text>
+            <Text style={TextStyles.heading2}>
+              {i18nCtx.t("days")}:{" "}
+              <Text style={styles.body}>{i18nCtx.t("hours")}</Text>
+            </Text>
+
+            <Text />
+
+            <Text style={TextStyles.heading2}>
+              {i18nCtx.t("emailUs")}:{" "}
+              <Text
+                style={[styles.body, styles.link]}
+                onPress={() => Linking.openURL("mailto:info@wordsalive.org")}
+              >
+                info@wordsalive.org
+              </Text>
+            </Text>
+
+            <Text />
+
+            <Text style={TextStyles.heading2}>{i18nCtx.t("address")}:</Text>
+
+            {/*We would use separate <Text> tags instead of \n's here, but we want the entire address to be selectable as one element so it can be copied or opened in Maps*/}
+            <Text style={styles.body} selectable={true}>
+              5111 Santa Fe Street Suite 219{"\n"}San Diego, California, 92109
+              {"\n"}
+              United States
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            onPress={() => setShowMoreHelp(false)}
+            style={styles.closeButtonContainer}
+          >
+            <Image
+              style={styles.closeButtonIcon}
+              source={require("../../assets/images/times-solid.png")}
+            />
+          </TouchableOpacity>
+        </View>
+      ) : (
+        null
+      )}
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  contentContainer: {
+    backgroundColor: Colors.orange,
+    width: "100%",
+    height: "100%",
+    justifyContent: "space-between",
+    flexDirection: "column",
+  },
+  headerContainer: {
+    alignItems: "center",
+  },
+  chatContainer: {
+    height: "100%",
+    backgroundColor: Colors.white,
+    justifyContent: "space-between",
+    marginBottom: -36,
+  },
+  leftChatBubble: {
+    backgroundColor: Colors.lightGray,
+    padding: 8,
+  },
+  rightChatBubble: {
+    backgroundColor: Colors.orange,
+    padding: 8,
+  },
+  messageInput: {
+    flex: 1,
+    borderWidth: 2,
+    borderColor: Colors.lightGray,
+    borderRadius: 5,
+    padding: 8,
+    marginBottom: 20,
+  },
+  newMessageContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 20,
+  },
+  sendButtonContainer: {
+    height: 38,
+    width: 60,
+    marginLeft: 8,
+    borderRadius: 5,
+    backgroundColor: Colors.orange,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sendIcon: {
+    tintColor: Colors.white,
+    width: 22,
+    height: 22,
+  },
   logoContainer: {
-    width: '100%',
+    width: "100%",
     height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginBottom: 50,
   },
   logo: {
@@ -178,21 +268,36 @@ const styles = StyleSheet.create({
   },
   link: {
     color: Colors.link,
-    textDecorationLine: 'underline',
+    textDecorationLine: "underline",
   },
-  container: {
-    flex: 1,
-    alignItems: 'flex-start',
-    justifyContent: 'center',
+  moreHelpContainer: {
+    position: "absolute",
+    justifyContent: "space-around",
+    top: 0,
+    bottom: 0,
+    alignItems: "flex-start",
     backgroundColor: Colors.orange,
-    width: '100%',
-    height: '100%',
+    width: "100%",
     paddingLeft: 24,
     paddingRight: 24,
   },
+  closeButtonContainer: {
+    alignSelf: "center",
+    width: 40,
+    height: 40,
+    backgroundColor: Colors.white,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  closeButtonIcon: {
+    height: 20,
+    width: 20,
+    tintColor: Colors.orange,
+  },
   body: {
     fontSize: 18,
-    fontFamily: 'Gotham-Light',
+    fontFamily: "Gotham-Light",
     color: Colors.text,
   },
 });
